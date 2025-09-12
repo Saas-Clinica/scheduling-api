8b9f9e67215ccecadedc11e35657ce8feba43f5c
icaro.o.macedo <icaro.o.macedo@gmail.com>
2025-09-12 11:31:22 -0300

    initialyzing repository

---
+++ src/scheduling-api/Adapters/Driving/scheduling-api/Controllers/v1/AppointmentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Controllers.v1;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[SwaggerTag("Agendamentos de procedimentos")]
+public class AppointmentController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+    [ProducesResponseType(StatusCodes.Status503ServiceUnavailable)]
+    [SwaggerOperation(
+        Summary = "Cria um novo agendamento de procedimento",
+        Description = "Cria um novo agendamento de procedimento para um paciente em uma data e hora específicas."
+    )]
+
+    public Task<IActionResult> CreateAppointment(
+        
+        )
+    {
+         return Task.FromResult<IActionResult>(Ok());
+    }
+
+
+    //TODO: POST   /api/appointments          -> criar
+    //TODO: GET    /api/appointments
+    //TODO: GET    /api/appointments/{id}     -> detalhe
+    //TODO: PUT    /api/appointments/{id}     -> atualizar (ex.: reagendar)
+    //TODO: DELETE /api/appointments/{id}     -> cancelar
+
+    //TODO: POST   /api/appointments/{id}/confirm  
+    //TODO: POST   /api/appointments/{id}/cancel
+    //TODO: POST   /api/appointments/{id}/reschedule
+}
---
+++ src/scheduling-api/Adapters/Driving/scheduling-api/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(
+    cfg => cfg.EnableAnnotations()
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
